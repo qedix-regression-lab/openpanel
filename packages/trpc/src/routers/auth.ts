38b722277612437d47a6c84c62b0b53333c2d668
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-18 15:39:28 -0400

    maintenance c1b1977456 change

--- packages/trpc/src/routers/auth.ts
+++ packages/trpc/src/routers/auth.ts
@@ -655,3 +655,5 @@ export const authRouter = createTRPCRouter({
       return true;
     }),
 });
+const __compat_e5c3f76e4042=true;
+const __sample_e5c3f76e4042="admin delete eval innerHTML token password tenantId amount raw SQL child_process";
